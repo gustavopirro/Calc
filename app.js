99113f761078f38882e2204b588a0a642d6ffb3d
unknown <gustavo.pirro@gmail.com>
2020-08-26 23:24:01 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,79 @@
+let number1, number2, lastOperation, memoryFull = 0;
+
+function button(n) {
+    if (memoryFull != 1) {
+       number1 = document.getElementById("visor").innerHTML + n;
+        document.getElementById("visor").innerText = number1;
+    } else {
+        number2 = document.getElementById("visor").innerHTML + n;
+        document.getElementById("visor").innerText = number2;
+    }
+}
+function sum(x,y){ return x + y }
+function sub(x,y){ return x - y }
+function div(x,y){ return x / y }
+function mult(x,y){ return x * y }
+
+function operation(op) {
+    function isMemoryFull() {
+        if (memoryFull === 1) {
+            number1 = op((number1), (number2));
+            number2 = undefined;
+            document.getElementById("visor").innerText = number1;
+        } else {
+            memoryFull = 1;
+        }
+    }
+    lastOperation = op
+    isMemoryFull();
+    document.getElementById("visor").innerText = ""
+}
+
+function equal() {
+    if (number1 !== undefined) {
+        memoryFull = 0;
+        switch (lastOperation) {
+            case sum:
+                if (number2 == undefined) {
+                    document.getElementById("visor").innerText = parseFloat(number1)
+                } else {
+                    number1 = parseFloat(number1) + parseFloat(number2);
+                }
+                document.getElementById("visor").innerText = number1;
+                number2 = undefined;
+                break;
+                
+            case sub:
+                if (number2 == undefined) {
+                    document.getElementById("visor").innerText = parseFloat(number1)
+                } else {
+                    number1 = parseFloat(number1) - parseFloat(number2);
+                }
+                document.getElementById("visor").innerText = number1;
+                number2 = undefined;
+                break;
+
+            case mult:
+                if (number2 == undefined) {
+                    document.getElementById("visor").innerText = parseFloat(number1)
+                } else {
+                    number1 = parseFloat(number1) * parseFloat(number2);
+                }
+                document.getElementById("visor").innerText = number1;
+                number2 = undefined;
+                break;
+
+            case div:
+                if (number2 == undefined) {
+                    document.getElementById("visor").innerText = parseFloat(number1)
+                } else {
+                    number1 = parseFloat(number1) / parseFloat(number2);
+                }
+                document.getElementById("visor").innerText = number1;
+                number2 = undefined;
+                break;
+                
+        }
+
+    }
+}
